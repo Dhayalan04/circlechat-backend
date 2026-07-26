fef89e876c3fc8d9bcc920fb3ad35ecb4b4b569f
dhayalan <dhayalanspace@gmail.com>
2026-07-26 14:46:18 +0530

    initial mangoDB setup

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
+const mongoose = require("mongoose");
 const express = require("express");
+require("dotenv").config();
 
 const app = express();
+app.use(express.json());
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
+
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected successfully");
+    })
+    .catch((error) => {
+        console.log("MongoDB connection error:", error);
+    });
 
 app.get("/", (req, res) => {
     res.send("Server is running successfully !");
